51f3bef0544157cc1527cea6f2fca2a5d062fea8
A5Beef <edvintran@gmail.com>
2025-11-04 19:13:51 +0200

    exc1 2 3

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,29 @@
 from player import Player
+import requests
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+
+        if player.nationality == "FIN":
+            players.append(player)
+    
+    players.sort(key=lambda p: p.goals + p.assists, reverse=True)
+
+    print("Players from FIN")
+    print()
+
+    for player in players:
+        print(player)
+
 
 if __name__ == "__main__":
     main()
