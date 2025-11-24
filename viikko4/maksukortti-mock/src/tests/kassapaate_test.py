20c59c295917c6592a59bb8c02d36bf6463325c4
A5Beef <edvintran@gmail.com>
2025-11-24 22:39:26 +0200

    exc 2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataa_kutsu_lisaa_rahaa_jos_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 0
+
+        self.kassa.lataa(maksukortti_mock, 20)
+
+        maksukortti_mock.lataa.assert_called_with(20)
+
+    def test_lataa_kutsu_ei_lisaa_rahaa_jos_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 0
+
+        self.kassa.lataa(maksukortti_mock, -10)
+
+        maksukortti_mock.lataa.assert_not_called()
