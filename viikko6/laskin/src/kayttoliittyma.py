9d243ef49b3384a4fb67fe25f5700e99a143f795
A5Beef <154082807+A5Beef@users.noreply.github.com>
2025-12-09 19:32:18 +0200

    Add files via upload

--- viikko6/laskin/src/kayttoliittyma.py
+++ viikko6/laskin/src/kayttoliittyma.py
@@ -9,10 +9,86 @@ class Komento(Enum):
     KUMOA = 4
 
 
+class Komentotehdas:
+    def __init__(self, sovelluslogiikka, kayttoliittyma):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._kayttoliittyma = kayttoliittyma
+        self._komennot = {}
+        self._alusta_komennot()
+    
+    def _alusta_komennot(self):
+        self._komennot[Komento.SUMMA] = Summa(
+            self._sovelluslogiikka, 
+            lambda: self._lue_syote()
+        )
+        self._komennot[Komento.EROTUS] = Erotus(
+            self._sovelluslogiikka, 
+            lambda: self._lue_syote()
+        )
+        self._komennot[Komento.NOLLAUS] = Nollaus(self._sovelluslogiikka)
+    
+    def _lue_syote(self):
+        try:
+            return int(self._kayttoliittyma._syote_kentta.get())
+        except Exception:
+            return 0
+    
+    def hae(self, komento):
+        return self._komennot.get(komento)
+
+
+class Summa:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+        self._edellinen_arvo = 0
+    
+    def suorita(self):
+        self._edellinen_arvo = self._sovelluslogiikka.arvo()
+        syote = self._lue_syote()
+        self._sovelluslogiikka.plus(syote)
+        return self._edellinen_arvo
+    
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_arvo)
+
+
+class Erotus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+        self._edellinen_arvo = 0
+    
+    def suorita(self):
+        self._edellinen_arvo = self._sovelluslogiikka.arvo()
+        syote = self._lue_syote()
+        self._sovelluslogiikka.miinus(syote)
+        return self._edellinen_arvo
+    
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_arvo)
+
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._edellinen_arvo = 0
+    
+    def suorita(self):
+        self._edellinen_arvo = self._sovelluslogiikka.arvo()
+        self._sovelluslogiikka.nollaa()
+        return self._edellinen_arvo
+    
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_arvo)
+
+
 class Kayttoliittyma:
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+        self._komentotehdas = Komentotehdas(sovelluslogiikka, self)
+        self._historia = []
 
     def kaynnista(self):
         self._arvo_var = StringVar()
@@ -55,28 +131,32 @@ class Kayttoliittyma:
         self._kumoa_painike.grid(row=2, column=3)
 
     def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
-
-        self._kumoa_painike["state"] = constants.NORMAL
-
+        if komento == Komento.KUMOA:
+            self._kumoa()
+        else:
+            komento_olio = self._komentotehdas.hae(komento)
+            if komento_olio:
+                self._historia.append(komento_olio)
+                komento_olio.suorita()
+        
+        self._paivita_napit()
+        self._syote_kentta.delete(0, constants.END)
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
+    
+    def _kumoa(self):
+        if self._historia:
+            viimeisin_komento = self._historia.pop()
+            viimeisin_komento.kumoa()
+    
+    def _paivita_napit(self):
+        # Kumoa-painikkeen tila
+        if self._historia:
+            self._kumoa_painike["state"] = constants.NORMAL
+        else:
+            self._kumoa_painike["state"] = constants.DISABLED
+        
+        # Nollaus-painikkeen tila
         if self._sovelluslogiikka.arvo() == 0:
             self._nollaus_painike["state"] = constants.DISABLED
         else:
             self._nollaus_painike["state"] = constants.NORMAL
-
-        self._syote_kentta.delete(0, constants.END)
-        self._arvo_var.set(self._sovelluslogiikka.arvo())
